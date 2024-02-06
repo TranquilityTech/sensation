71f64ccda37298baaea261596ccc546401f14e94
Ta-Yi Cheng <admin@tranquility.tech>
2024-02-06 13:33:31 +0800

    feat: add onTap onDoubleTap onLongPress callback

--- lib/sensation.dart
+++ lib/sensation.dart
@@ -2,11 +2,31 @@ library sensation;
 
 import 'package:flutter/material.dart';
 
-class Sensation extends StatelessWidget {
-  const Sensation({super.key});
+class Sensation extends StatefulWidget {
+  final Widget child;
+  final VoidCallback? onTap;
+  final VoidCallback? onDoubleTap;
+  final VoidCallback? onLongPress;
+
+  const Sensation({
+    super.key,
+    required this.child,
+    this.onTap,
+    this.onDoubleTap,
+    this.onLongPress,
+  });
 
   @override
+  State<Sensation> createState() => SensationState();
+}
+
+class SensationState extends State<Sensation> {
+  @override
   Widget build(BuildContext context) {
-    return const Text("Hello World");
+    return GestureDetector(
+        onTap: widget.onTap,
+        onDoubleTap: widget.onDoubleTap,
+        onLongPress: widget.onLongPress,
+        child: widget.child);
   }
 }
